7729b59ad1dd960b5b23a0e12fd180fd57e330c2
Anna <104223316+lastet@users.noreply.github.com>
2022-06-22 13:37:38 -0500

    initial commit

---
+++ src/app.js
@@ -0,0 +1,9 @@
+const hamButton = document.getElementById('hamburger')
+const navList = document.getElementById('nav-list')
+
+function toggleButton(){
+    navList.classList.toggle('show')
+
+}
+
+hamButton.addEventListener('click', toggleButton)
